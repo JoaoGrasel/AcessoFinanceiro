5d93dd72ee92f82e166fea90244cdd64328d90b4
Thiago Brezinski <thiagobrez@gmail.com>
2017-09-30 19:58:05 -0300

    Atualizado ControladorAcesso, TelaAcesso, ControladorRelatorioAcesso, TelaRelatorioAcesso e Constantes.

--- AcessoFinanceiro/src/br/ufsc/ine5605/acessofinanceiro/Constantes.java
+++ AcessoFinanceiro/src/br/ufsc/ine5605/acessofinanceiro/Constantes.java
@@ -12,18 +12,25 @@ package br.ufsc.ine5605.acessofinanceiro;
  * @author thiagobrezinski
  */
 class Constantes {
+    // COMUM
+    public static final String INSIRA_MATRICULA = "Insira a matricula: ";
+    public static final String MATRICULA_INVALIDA = "Matricula invalida: a matricula deve conter somente numeros.";
+    public static final String OPCAO_INVALIDA = "Opcao invalida: por favor digite somente um numero.";
+    public static final String OPCAO_INEXISTENTE = "Opcao inexistente: tente novamente.";
     
     // ACESSO AO FINANCEIRO
     public static final String ACESSO_FINANCEIRO = "+-+-+-+-+-+-+-+ Acesso ao financeiro +-+-+-+-+-+-+-+";
-    public static final String INSIRA_MATRICULA = "Insira sua matricula: ";
-    public static final String MATRICULA_INVALIDA = "Matricula invalida: a matricula deve conter somente numeros.";
-    public static final String MATRICULA_INEXISTENTE = "Nao foi possivel acessar o financeiro. A matricula inserida nao existe.";
-    public static final String ACESSO_PERMITIDO = "Parabens, voce conseguiu acessar o financeiro. Aproveite!";
+    public static final String ACESSO_MATRICULA_INEXISTENTE = "Nao foi possivel acessar o financeiro. A matricula inserida nao existe.";
+    public static final String ACESSO_PERMITIDO = "Parabens, voce conseguiu acessar o financeiro. Faca a festa!";
     
-    //RELATORIO
+    // RELATORIO
     public static final String RELATORIO_ACESSO = "+-+-+-+-+-+-+-+ Relatorio de acesso negado +-+-+-+-+-+-+-+";
     public static final String RELATORIO_ESCOLHA_FILTRO = "Escolha o filtro: ";
     public static final String RELATORIO_FILTRO_MOTIVO = "1 - Por motivo";
     public static final String RELATORIO_FILTRO_MATRICULA = "2 - Por matricula";
-    public static final String RELATORIO_FILTRO_INVALIDO = "Opcao invalida: por favor digite somente numeros.";
+    public static final String RELATORIO_FILTRO_ESCOLHA_MOTIVO = "Escolha o motivo: ";
+    public static final String RELATORIO_FILTRO_MOTIVO_MATRICULA_INEXISTENTE = "1 - Matricula inexistente";
+    public static final String RELATORIO_FILTRO_MOTIVO_CARGO_SEM_ACESSO = "2 - Cargo sem acesso";
+    public static final String RELATORIO_FILTRO_MOTIVO_HORARIO_NAO_PERMITIDO = "3 - Horario nao permitido";
+    public static final String RELATORIO_FILTRO_MOTIVO_ACESSO_BLOQUEADO = "4 - Acesso bloqueado";
 }
